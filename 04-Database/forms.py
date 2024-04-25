103eb5de0701a5b3d4d0755fec1ce2a42026ce7a
paschalugwu <ugwupaschal@gmail.com>
2024-04-25 11:47:32 +0100

    feat(forms.py): Implement user registration and login forms
    Implemented RegistrationForm and LoginForm classes in forms.py to handle user registration and login functionality. Added form fields for username, email, password, and confirmation password, along with validation logic. Integrated WTForms for form validation and CSRF protection.

--- 04-Database/forms.py
+++ 04-Database/forms.py
@@ -5,19 +5,39 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
+    """
+    Form class for user registration.
+
+    Fields:
+    - username: StringField for username (required, \
+        length between 2 and 20 characters)
+    - email: StringField for email (required, valid email format)
+    - password: PasswordField for password (required)
+    - confirm_password: PasswordField for confirming \
+        password (required, must match 'password' field)
+    - submit: SubmitField for submitting the form
+    """
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+                                     validators=[DataRequired(),
+                                                 EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+    """
+    Form class for user login.
+
+    Fields:
+    - email: StringField for email (required, valid email format)
+    - password: PasswordField for password (required)
+    - remember: BooleanField for remembering user session
+    - submit: SubmitField for submitting the form
+    """
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
