f3c5b9ded29dab244f1c73a62dd76e92b98c75fb
paschalugwu <ugwupaschal@gmail.com>
2024-04-25 15:12:49 +0100

    chore(init): Initialize flask_ambrosial package
    Created init.py file to initialize the flask_ambrosial package and set up the Flask application

--- 05-Package-Structure/flask_ambrosial/__init__.py
+++ 05-Package-Structure/flask_ambrosial/__init__.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
+
+# Import necessary modules
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# Create a Flask application instance
 app = Flask(__name__)
+
+# Set Flask app configurations
+# - SECRET_KEY is used to keep data secure (e.g., for form submissions)
+# - SQLALCHEMY_DATABASE_URI specifies the location of the SQLite database file
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# Create a SQLAlchemy database instance, linking it to the Flask app
 db = SQLAlchemy(app)
 
+# Import routes from the flask_ambrosial package
+# This imports routes and registers them with the Flask app
 from flask_ambrosial import routes
