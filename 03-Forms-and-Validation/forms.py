20a3080ede90afa5e4f989878fb16ffb841e1d3b
paschalugwu <ugwupaschal@gmail.com>
2024-04-25 10:33:08 +0100

    feat(forms): Implement form validation in forms.py
    Added form validation logic to ensure that user inputs are properly validated according to specified requirements. This enhances the security and integrity of user data submitted through the forms on the website.

--- 03-Forms-and-Validation/forms.py
+++ 03-Forms-and-Validation/forms.py
@@ -5,17 +5,37 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
+    """
+    Form for user registration.
+
+    Attributes:
+        username (StringField): Field for entering username.
+        email (StringField): Field for entering email.
+        password (PasswordField): Field for entering password.
+        confirm_password (PasswordField): Field for confirming password.
+        submit (SubmitField): Button for submitting registration form.
+    """
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+                                     validators=[DataRequired(),
+                                                 EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
+    """
+    Form for user login.
+
+    Attributes:
+        email (StringField): Field for entering email.
+        password (PasswordField): Field for entering password.
+        remember (BooleanField): Checkbox for remembering user login.
+        submit (SubmitField): Button for submitting login form.
+    """
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
