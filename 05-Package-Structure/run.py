a7ce4ec65ed11c4cec3493d76239fc0d42cc0447
paschalugwu <ugwupaschal@gmail.com>
2024-04-25 14:55:02 +0100

    feat(run.py): Add explanatory comments and docstrings
    Added explanatory comments and docstrings to the run.py file to improve code readability and provide clear documentation on the interaction with the Flask app.

--- 05-Package-Structure/run.py
+++ 05-Package-Structure/run.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
+
+# Import the Flask application instance from flask_ambrosial package
 from flask_ambrosial import app
 
+# Check if the script is being run directly by the Python interpreter
 if __name__ == '__main__':
+    """ Run the Flask application
+     - debug=True enables debug mode, allowing for live code \
+         reloading and better error messages
+     - This is the entry point of the application, where Flask \
+         starts listening for requests
+    """
     app.run(debug=True)
