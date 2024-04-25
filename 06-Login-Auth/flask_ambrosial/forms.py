a077bfb7cd8990ce15642ca4956a05ab923f3330
paschalugwu <ugwupaschal@gmail.com>
2024-04-25 15:57:56 +0100

    feat(forms): Add form validation and fields
    Added form validation and fields for user registration and login in flask_ambrosial/forms.py.

--- 06-Login-Auth/flask_ambrosial/forms.py
+++ 06-Login-Auth/flask_ambrosial/forms.py
@@ -3,12 +3,14 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, \
+    Length, Email, EqualTo, ValidationError
 from flask_ambrosial.models import User
+
 
 class RegistrationForm(FlaskForm):
     """Form for user registration."""
-    
+
     # Fields for username, email, password, and confirm password
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
@@ -16,7 +18,8 @@ class RegistrationForm(FlaskForm):
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+                                     validators=[DataRequired(),
+                                                 EqualTo('password')])
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
@@ -25,7 +28,8 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(username=username.data).first()
         if user:
             # If a user with this username exists, raise a validation error
-            raise ValidationError('That username is taken. Please choose a different one.')
+            raise ValidationError('That username is taken. \
+                Please choose a different one.')
 
     def validate_email(self, email):
         """Validation to check if email is already registered."""
@@ -33,11 +37,13 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             # If a user with this email exists, raise a validation error
-            raise ValidationError('That email is taken. Please choose a different one.')
+            raise ValidationError('That email is taken. \
+                Please choose a different one.')
+
 
 class LoginForm(FlaskForm):
     """Form for user login."""
-    
+
     # Fields for email, password, remember me checkbox, and submit button
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
