ea481651393ab754162c046baa0f3dd7557c1e16
paschalugwu <ugwupaschal@gmail.com>
2024-04-25 15:55:52 +0100

    feat(routes): Add routes for home, about, register, and login
    Added routes for the home page, about page, user registration, and user login in flask_ambrosial/routes.py.

--- 06-Login-Auth/flask_ambrosial/routes.py
+++ 06-Login-Auth/flask_ambrosial/routes.py
@@ -12,16 +12,33 @@ posts = [
     {
         'author': 'Ugwu Paschal',
         'title': 'How to prepare Jollof Rice.',
-        'content': 'Jollof rice, a beloved West African dish, is a flavorful one-pot meal. Made from rice, tomatoes, onions, peppers, and aromatic spices, it’s a staple during holidays, weddings, and special events. Customize it with your favorite veggies and pair it with fried plantains for a delightful feast!',
+        'content': 'Jollof rice, a beloved West African dish, \
+            is a flavorful one-pot meal. Made from rice, \
+                tomatoes, onions, peppers, and aromatic spices, \
+                    it’s a staple during holidays, weddings, \
+                        and special events. Customize it with \
+                            your favorite veggies and pair it \
+                                with fried plantains for a \
+                                    delightful feast!',
         'date_posted': 'April 18, 2024'
     },
     {
         'author': 'Amarachi Nnanta',
         'title': 'Prepare Egusi Soup Like A Pro.',
-        'content': 'Egusi soup, a Nigerian delicacy, is rich and flavorful. To prepare it like a pro, heat oil in a pot and sauté onions. Add ingredients (except ground egusi) and boil for 3 minutes. Stir in ground egusi, adjusting water for desired consistency. Cook on low heat for 15-20 minutes, and serve with yam, rice, or grilled meat.',
+        'content': 'Egusi soup, a Nigerian delicacy, \
+            is rich and flavorful. To prepare it like a pro, \
+                heat oil in a pot and sauté onions. Add \
+                    ingredients (except ground egusi) and boil \
+                        for 3 minutes. Stir in ground egusi, \
+                            adjusting water for desired \
+                                consistency. Cook on low heat \
+                                    for 15-20 minutes, and \
+                                        serve with yam, rice, \
+                                            or grilled meat.',
         'date_posted': 'April 19, 2024'
     }
 ]
+
 
 @app.route("/")
 @app.route("/home")
@@ -29,10 +46,12 @@ def home():
     """Render the home page with posts."""
     return render_template('home.html', posts=posts)
 
+
 @app.route("/about")
 def about():
     """Render the about page."""
     return render_template('about.html', title='About')
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -42,15 +61,19 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         # Hash the password before storing it
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        hashed_password = bcrypt.generate_password_hash(form.password.data)\
+            .decode('utf-8')
         # Create a new User object
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
         # Add the user to the database session
         db.session.add(user)
         db.session.commit()
-        flash(f'Your account has been created! You are now able to log in', 'success')
+        flash(f'Your account has been created! \
+            You are now able to log in', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -61,20 +84,25 @@ def login():
     if form.validate_on_submit():
         # Check if the user exists and the password is correct
         user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+        if user and bcrypt.check_password_hash(user.password,
+                                               form.password.data):
             # Log in the user
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+            return redirect(next_page) if next_page else \
+                redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
+            flash('Login Unsuccessful. \
+                Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
 
 @app.route("/logout")
 def logout():
     """Handle user logout."""
     logout_user()
     return redirect(url_for('home'))
+
 
 @app.route("/account")
 @login_required
