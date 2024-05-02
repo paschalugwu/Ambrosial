b6e6d5dd30d374e05c965c6ebd5a9d122fd8f484
paschalugwu <ugwupaschal@gmail.com>
2024-05-02 13:58:29 +0100

    feat(forms): Add user registration, login, and account update forms
    Added Flask forms for user registration, login, and account update. These forms include validation for username and email to ensure uniqueness during registration and account updates.

--- 07-User-Account-Profile-Pic/flask_ambrosial/forms.py
+++ 07-User-Account-Profile-Pic/flask_ambrosial/forms.py
@@ -1,52 +1,80 @@
 #!/usr/bin/python3
-"""Forms for user registration and login."""
+"""Forms for user registration, login, and account update."""
 
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, \
-    Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_ambrosial.models import User
-
 
 class RegistrationForm(FlaskForm):
     """Form for user registration."""
 
-    # Fields for username, email, password, and confirm password
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(),
-                                                 EqualTo('password')])
-    submit = SubmitField('Sign Up')
-
     def validate_username(self, username):
-        """Validation to check if username is already taken."""
-        # Query the database for a user with the same username
+        """
+        Validate if the username is already taken.
+
+        Args:
+            username (str): The username entered by the user.
+
+        Raises:
+            ValidationError: If a user with the same username already exists.
+        """
         user = User.query.filter_by(username=username.data).first()
         if user:
-            # If a user with this username exists, raise a validation error
             raise ValidationError('That username is taken. \
                 Please choose a different one.')
 
     def validate_email(self, email):
-        """Validation to check if email is already registered."""
-        # Query the database for a user with the same email
+        """
+        Validate if the email is already registered.
+
+        Args:
+            email (str): The email entered by the user.
+
+        Raises:
+            ValidationError: If a user with the same email already exists.
+        """
         user = User.query.filter_by(email=email.data).first()
         if user:
-            # If a user with this email exists, raise a validation error
             raise ValidationError('That email is taken. \
                 Please choose a different one.')
-
 
 class LoginForm(FlaskForm):
     """Form for user login."""
 
-    # Fields for email, password, remember me checkbox, and submit button
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+class UpdateAccountForm(FlaskForm):
+    """Form for updating user account."""
+
+    def validate_username(self, username):
+        """
+        Validate if the username is already taken during account update.
+
+        Args:
+            username (str): The username entered by the user.
+
+        Raises:
+            ValidationError: If a user with the same username already exists.
+        """
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. \
+                    Please choose a different one.')
+
+    def validate_email(self, email):
+        """
+        Validate if the email is already registered during account update.
+
+        Args:
+            email (str): The email entered by the user.
+
+        Raises:
+            ValidationError: If a user with the same email already exists.
+        """
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. \
+                    Please choose a different one.')
