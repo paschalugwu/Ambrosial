76c69d098e69f7ecd052908639fcd6cc03bc19f1
paschalugwu <ugwupaschal@gmail.com>
2024-05-06 14:28:28 +0100

    feat(flask_ambrosial): Update form validation
    This commit includes updates to the form validation logic in the forms.py file. The changes improve the user experience by providing more informative error messages and robust input validation.

--- 08-Posts/flask_ambrosial/forms.py
+++ 08-Posts/flask_ambrosial/forms.py
@@ -8,80 +8,59 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextA
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_ambrosial.models import User
 
+
 class RegistrationForm(FlaskForm):
-    """Form for user registration."""
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-        """
-        Validate if the username is already taken.
-
-        Args:
-            username (str): The username entered by the user.
-
-        Raises:
-            ValidationError: If a user with the same username already exists.
-        """
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. \
-                Please choose a different one.')
+            raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        """
-        Validate if the email is already registered.
-
-        Args:
-            email (str): The email entered by the user.
-
-        Raises:
-            ValidationError: If a user with the same email already exists.
-        """
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is taken. \
-                Please choose a different one.')
+            raise ValidationError('That email is taken. Please choose a different one.')
+
 
 class LoginForm(FlaskForm):
-    """Form for user login."""
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
 
 class UpdateAccountForm(FlaskForm):
-    """Form for updating user account."""
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
 
     def validate_username(self, username):
-        """
-        Validate if the username is already taken during account update.
-
-        Args:
-            username (str): The username entered by the user.
-
-        Raises:
-            ValidationError: If a user with the same username already exists.
-        """
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError('That username is taken. \
-                    Please choose a different one.')
+                raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        """
-        Validate if the email is already registered during account update.
-
-        Args:
-            email (str): The email entered by the user.
-
-        Raises:
-            ValidationError: If a user with the same email already exists.
-        """
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError('That email is taken. \
-                    Please choose a different one.')
+                raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired])
-    content = TextAreaField('Content', validators=[DataRequired])
-    image_filename = FileField('Upload Image', validators=[FileAllowed(['jpg', 'png'])])
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    image_filename = FileField('Image', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
     submit = SubmitField('Post')
