bb4b47fcc95e8272923f38c5abf1ce9395463fd4
paschalugwu <ugwupaschal@gmail.com>
2024-05-06 21:48:48 +0100

    feat(routes): Implement pagination for home route
    Updated the home route in routes.py to include pagination for displaying posts.
    Utilized Flask's paginate method to fetch and display posts in batches on the home page.
    Improved user experience by enabling navigation between different pages of posts.

--- 09-Pagination/flask_ambrosial/routes.py
+++ 09-Pagination/flask_ambrosial/routes.py
@@ -14,7 +14,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
     image_files = []
     for post in posts:
         image_files.append(url_for('static', filename='post_pics/' + post.image_filename))
@@ -203,3 +204,16 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted', 'success')
     return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=3)
+    image_files = []
+    for post in posts:
+        image_files.append(url_for('static', filename='post_pics/' + post.image_filename))
+    return render_template('user_posts.html', posts=posts, image_files=image_files, user=user)
